2cb3e743f6ff345f79a08b68fbb54e7817c8f7f1
Michael F. Booth <michaelbooth@helpscout.com>
2019-01-11 01:13:07 -0500

    Add commentary

--- aws_factory.go
+++ aws_factory.go
@@ -7,20 +7,28 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 )
 
+// An AwsFactory wraps connections to the AWS API.
+//
+// Unit tests may use a fake factory that returns fake connections.
 type AwsFactory interface {
 	NewS3(...*aws.Config) s3iface.S3API
 }
 
+// DefaultAwsFactory returns real connections to the AWS API.
 type DefaultAwsFactory struct {
 	Session *session.Session
 }
 
+// NewAwsFactory accepts the same arguments as AWS's
+// `session.NewSession` and returns an AwsFactory that makes real
+// connections to AWS.
 func NewAwsFactory(cfgs ...*aws.Config) AwsFactory {
 	return &DefaultAwsFactory{
 		Session: session.Must(session.NewSession(cfgs...)),
 	}
 }
 
+// NewS3 return an AWS S3 client.
 func (f *DefaultAwsFactory) NewS3(cfgs ...*aws.Config) s3iface.S3API {
 	return s3.New(f.Session, cfgs...)
 }
--- copy_object.go
+++ copy_object.go
@@ -12,7 +12,7 @@ import (
 )
 
 // parseFullS3Url tries to parse the given string as an `s3://`
-// url. If it succeeds, it returns the S3 bucket and object names
+// URL. If it succeeds, it returns the S3 bucket and object names
 // along with `true`. Otherwise it returns empty strings and `false`.
 func parseS3Url(url string) (string, string, bool) {
 	re := regexp.MustCompile(`\As3://([^/]+)/([^/].*)`)
@@ -23,6 +23,9 @@ func parseS3Url(url string) (string, string, bool) {
 	return m[1], m[2], true
 }
 
+// urlReader returns a ReadCloser that will read from the given URL,
+// whether that URL points to s3 or is a path to a file on the local
+// disk.
 func urlReader(url string, awsFactory AwsFactory) (io.ReadCloser, error) {
 	b, o, isS3 := parseS3Url(url)
 	if isS3 {
@@ -39,11 +42,23 @@ func urlReader(url string, awsFactory AwsFactory) (io.ReadCloser, error) {
 	return os.Open(url)
 }
 
-func writeUrl(url string, data io.Reader, awsFactory AwsFactory) error {
+// writeUrl reads data from the given reader and writes it to the
+// given URL, whether that URL points to s3 or to a file on local
+// disk.
+func writeUrl(url string, reader io.Reader, awsFactory AwsFactory) error {
 	var targetFile *os.File
 	var err error
 	b, o, isS3 := parseS3Url(url)
 	if isS3 {
+		// To write to S3 we need an io.ReadSeeker, not a plain
+		// io.Reader (which does not support Seek()); this is probably
+		// because the S3 Put transaction needs to be signed, so it
+		// needs to scan the content twice: once to generate the
+		// signature, and once to send it over the network.
+		//
+		// So we copy the contents of the reader to a temporary file
+		// on disk, which will support Seek(), and then upload the
+		// temporary file to S3.
 		if targetFile, err = ioutil.TempFile(``, `just-s3-cp`); err != nil {
 			return err
 		}
@@ -53,12 +68,15 @@ func writeUrl(url string, data io.Reader, awsFactory AwsFactory) error {
 			return err
 		}
 	}
-	if _, err = io.Copy(targetFile, data); err != nil {
+	// Copy from the reader to the file on disk.
+	if _, err = io.Copy(targetFile, reader); err != nil {
 		return err
 	}
 	if !isS3 {
+		// The file on disk is the final destination; we're done
 		return nil
 	}
+	// Copy the temporary file to S3
 	if _, err = targetFile.Seek(0, 0); err != nil {
 		return err
 	}
@@ -70,11 +88,15 @@ func writeUrl(url string, data io.Reader, awsFactory AwsFactory) error {
 	return err
 }
 
+// copyObject copies the contents of the srcPath to the
+// destPath. These paths may be S3 URLs, or paths to files on the
+// local disk.
 func copyObject(srcPath string, destPath string, awsFactory AwsFactory) error {
 	fmt.Printf("Copying %s to %s\n", srcPath, destPath)
 	reader, err := urlReader(srcPath, awsFactory)
 	if err != nil {
 		return err
 	}
+	defer reader.Close()
 	return writeUrl(destPath, reader, awsFactory)
 }
